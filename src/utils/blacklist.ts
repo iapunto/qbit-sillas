74b7b683b85e7708c0a5a7cfdc06e20c80a81ef5
Desarrollo IA Punto <desarrollo@iapunto.com>
2025-05-20 14:42:11 -0500

    Blacklist Actualizado

--- src/utils/blacklist.ts
+++ src/utils/blacklist.ts
@@ -1,32 +1,33 @@
 export class Blacklist {
-    private static instance: Blacklist;
-    private blacklist: Set<string>;
-    private adminNumber: string;
+  private static instance: Blacklist;
+  private blacklist: Set<string>;
+  private adminNumber: string;
 
-    private constructor(adminNumber: string) {
-        this.blacklist = new Set();
-        this.adminNumber =  adminNumber;
-    }
+  private constructor(adminNumber: string) {
+    this.blacklist = new Set();
+    this.adminNumber = adminNumber;
+  }
 
-    public static getInstance(adminNumber: string): Blacklist {
-        if (!Blacklist.instance) {
-            Blacklist.instance = new Blacklist(adminNumber);
-        }
-        return Blacklist.instance;
+  public static getInstance(adminNumber: string): Blacklist {
+    if (!Blacklist.instance) {
+      Blacklist.instance = new Blacklist(adminNumber);
     }
+    return Blacklist.instance;
+  }
 
-    public add(number: string): void {
-        this.blacklist.add(number);
-    }
+  public add(number: string): void {
+    this.blacklist.add(number);
+  }
 
-    public remove(number: string): void {
-        this.blacklist.delete(number);
-    }
+  public remove(number: string): void {
+    this.blacklist.delete(number);
+  }
 
-    public has(number: string): boolean {
-        return this.blacklist.has(number);
-    }
+  public has(number: string): boolean {
+    return this.blacklist.has(number);
+  }
 
-    public isAdmin(number: string): boolean {
-        return number === this.adminNumber;
-    }
+  public isAdmin(number: string): boolean {
+    return number === this.adminNumber;
+  }
+}
